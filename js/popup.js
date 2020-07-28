f2728c8428a0722b947021e60fc0e6576174b626
Sanjay Rajmohan <sarajmoh@microsoft.com>
2020-07-27 21:40:57 -0700

    Add and delete card information

--- js/popup.js
+++ js/popup.js
@@ -3,6 +3,15 @@ let aliceblue = document.getElementById('aliceblue');
 let cornsilk = document.getElementById('cornsilk');
 let reset = document.getElementById('reset');
 
+let submitBtn = document.getElementById("submitBtn");
+let subNameInput = document.getElementById("sub-name");
+let cardNumberInput = document.getElementById("cardNumber");
+let cardExpiryInput = document.getElementById("cardExpiry");
+let cardCVCInput = document.getElementById("cardCVC");
+let db;
+
+var currTarget = "---";
+
 // use tabs.insertCSS to change header color on button click
 
 const source = document.querySelector("#card-type");
@@ -12,6 +21,7 @@ const target2 = document.querySelector(".new-cards")
 const displayExistingCardsWhenSelected = (source, value, target1) => {
     const selectedIndex = source.selectedIndex;
     const isSelected = source[selectedIndex].value === value;
+    currTarget = (isSelected) ? "target1" : currTarget;
     target1.classList[isSelected
         ? "add"
         : "remove"
@@ -20,6 +30,7 @@ const displayExistingCardsWhenSelected = (source, value, target1) => {
 const displayNewCardsWhenSelected = (source, value, target2) => {
     const selectedIndex = source.selectedIndex;
     const isSelected = source[selectedIndex].value === value;
+    currTarget = (isSelected) ? "target2" : currTarget;
     target2.classList[isSelected
         ? "add"
         : "remove"
@@ -32,3 +43,68 @@ displayExistingCardsWhenSelected(source, "existing", target1)
 source.addEventListener("change", (evt) =>
 displayNewCardsWhenSelected(source, "new-option", target2)
 );
+
+window.onload = function() {
+    let request = window.indexedDB.open('cred_it_db', 1);
+    request.onerror = function() {
+        console.log('Database failed to open');
+    };
+    
+    request.onsuccess = function() {
+        console.log('Database opened successfully');
+        db = request.result;
+    };
+
+    request.onupgradeneeded = function(e) {
+        let db = e.target.result;
+        let objectStore = db.createObjectStore('cred_it_os', { keyPath: 'id', autoIncrement:true });
+      
+        objectStore.createIndex('subName', 'subName', { unique: false });
+        objectStore.createIndex('cardNumber', 'cardNumber', { unique: false });
+        objectStore.createIndex('cardExpiry', 'cardExpiry', { unique: false });
+        objectStore.createIndex('cardCVC', 'cardCVC', { unique: false });
+      
+        console.log('Database setup complete');
+    };
+};
+
+submitBtn.addEventListener("click", () =>
+    addItem()
+);
+
+function addItem() {
+    if (subNameInput.value === "" || cardNumberInput.value === "" || cardExpiryInput.value === "" || cardCVCInput.value === "") {
+        console.log("Values cannot be empty");
+        return;
+    }
+
+    let newItem = { subName: subNameInput.value, cardNumber: cardNumberInput.value, cardExpiry: cardExpiryInput.value, cardCVC: cardCVCInput.value };
+    let transaction = db.transaction(['cred_it_os'], 'readwrite');
+    let objectStore = transaction.objectStore('cred_it_os');
+  
+    let request = objectStore.add(newItem);
+    request.onsuccess = function() {
+        subNameInput.value = "";
+        cardNumberInput.value = "";
+        cardExpiryInput.value = "";
+        cardCVCInput.value = "";
+    };
+  
+    transaction.oncomplete = function() {
+      console.log('Transaction completed: Successfully added new item to database.');
+    };
+  
+    transaction.onerror = function() {
+      console.log('Transaction not opened due to error');
+    };
+}
+
+function deleteItem(itemId) {
+    let transaction = db.transaction(['cred_it_os'], 'readwrite');
+    let objectStore = transaction.objectStore('cred_it_os');
+    let request = objectStore.delete(itemId);
+  
+    transaction.oncomplete = function() {
+      console.log('Item ' + itemId + ' deleted.');
+    };
+}
