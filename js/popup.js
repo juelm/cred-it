c2d8f084c667f6355869572ae491b321a3a3d092
Rucha Rangnekar <rurangne@microsoft.com>
2020-07-27 17:50:38 -0700

    rough homepage UI

--- js/popup.js
+++ js/popup.js
@@ -5,17 +5,30 @@ let reset = document.getElementById('reset');
 
 // use tabs.insertCSS to change header color on button click
 
-// aliceblue
-aliceblue.onclick = () => {
-  browser.tabs.insertCSS({code: "body { background: aliceblue !important; }"});
+const source = document.querySelector("#card-type");
+const target1 = document.querySelector(".existing-cards");
+const target2 = document.querySelector(".new-cards")
+
+const displayExistingCardsWhenSelected = (source, value, target1) => {
+    const selectedIndex = source.selectedIndex;
+    const isSelected = source[selectedIndex].value === value;
+    target1.classList[isSelected
+        ? "add"
+        : "remove"
+    ]("show");
+};
+const displayNewCardsWhenSelected = (source, value, target2) => {
+    const selectedIndex = source.selectedIndex;
+    const isSelected = source[selectedIndex].value === value;
+    target2.classList[isSelected
+        ? "add"
+        : "remove"
+    ]("show");
 };
 
-// cornsilk
-cornsilk.onclick = () => {
-  browser.tabs.insertCSS({code: "body { background: cornsilk !important; }"});
-};
-
-// back to original
-reset.onclick = () => {
-  browser.tabs.insertCSS({code: "body { background: none !important; }"});
-};
+source.addEventListener("change", (evt) =>
+displayExistingCardsWhenSelected(source, "existing", target1)
+);
+source.addEventListener("change", (evt) =>
+displayNewCardsWhenSelected(source, "new-option", target2)
+);
